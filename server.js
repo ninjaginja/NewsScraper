7c5270ad16c4090a4132b737a501efbbbb979a73
Molly Wojcik <mollywojcik@gmail.com>
2018-04-03 21:35:33 -0600

    building out base server.js file, scrape not working yet

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var port = process.env.PORT || 3000;
+var PORT = process.env.PORT || 3000;
 
 // Initialize Express
 var app = express();
@@ -31,8 +31,42 @@ app.use(express.static("public"));
 // Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
-mongoose.connect("mongodb://localhost/week18Populater", {
+mongoose.connect("mongodb://localhost/newsScraper", {
   useMongoClient: true
 });
 
 // Routes
+app.get("/scrape", function(req, res) {
+  axios.get("https://www.theonion.com/").then(function(response) {
+    var $ = cheerio.load(response.data);
+
+    $("a.js_curation-click").each(function(i, element) {
+      // Save an empty result object
+      var result = {};
+
+      // Add the text and href of every link, and save them as properties of the result object
+      result.title = $(this)
+        .text();
+      result.link = $(this)
+        .attr("href");
+
+      // Create a new Article using the `result` object built from scraping
+      db.Article
+        .create(result)
+        .then(function(dbArticle) {
+          // If we were able to successfully scrape and save an Article, send a message to the client
+          res.send("Scrape Complete");
+        })
+        .catch(function(err) {
+          // If an error occurred, send it to the client
+          res.json(err);
+        });
+    });
+  });
+});
+
+
+// Start the server
+app.listen(PORT, function() {
+  console.log("App running on port " + PORT + "!");
+});
