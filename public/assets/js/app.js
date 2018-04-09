08dd5258f8d4d3c3f0ba7f50072f4b4124790b6c
Molly Wojcik <mollywojcik@gmail.com>
2018-04-08 20:41:45 -0600

    final commit, need to resolve save note and save article

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,9 +1,11 @@
 // Whenever someone clicks a p tag
-$(document).on("click", "p", function() {
+$(document).on("click", ".title", function() {
+  console.log("TESTING for on click event .title");
   // Empty the notes from the note section
-  $(".notes").empty();
+  $("#notes").empty();
   // Save the id from the p tag
   var thisId = $(this).attr("data-id");
+  // console.log(thisId);
 
   // Now make an ajax call for the Article
   $.ajax({
@@ -13,29 +15,32 @@ $(document).on("click", "p", function() {
     // With that done, add the note information to the page
     .done(function(data) {
       console.log(data);
-      // The title of the article
-      $("#notes").append("<h2>" + data.title + "</h2>");
-      // An input to enter a new title
-      $("#notes").append("<input id='titleinput' name='title' >");
+      // An input to enter a new note title
+      $("#notes").append("<div class='input-field col s12'><input id='titleinput' name='title'></div>");
       // A textarea to add a new note body
-      $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
+      $("#notes").append("<div class='input-field col s12'><textarea id='bodyinput' class='materialize-textarea' name='body'></textarea></div>");
       // A button to submit a new note, with the id of the article saved to it
-      $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
+      $("#notes").append("<a class='waves-effect waves-light btn savenote' data-id='" + data._id + "' id='updatenotebutton'>Update Note</a>");
 
       // If there's a note in the article
       if (data.note) {
+        console.log("Testing if note exists:" + data.note);
         // Place the title of the note in the title input
         $("#titleinput").val(data.note.title);
         // Place the body of the note in the body textarea
         $("#bodyinput").val(data.note.body);
+        // Change savenotebutton to updatenotebutton
+        $("#savenotebutton").empty().append("<a class='waves-effect waves-light btn savenote' data-id='" + data._id + "' id='updatenotebutton'>Update Note</a>");
       }
     });
 });
 
 // When you click the savenote button
-$(document).on("click", "#savenote", function() {
+$(".savenote").on("click", function() {
+
+  console.log("This is when a note is saved.")
   // Grab the id associated with the article from the submit button
-  var thisId = $(this).attr("data-id");
+  let thisId = $(this).attr("data-id");
 
   // Run a POST request to change the note, using what's entered in the inputs
   $.ajax({
@@ -51,9 +56,9 @@ $(document).on("click", "#savenote", function() {
     // With that done
     .done(function(data) {
       // Log the response
-      console.log(data);
+      console.log("DATA: ======= " + data);
       // Empty the notes section
-      $(".notes").empty();
+      $("#notes").empty();
     });
 
   // Also, remove the values entered in the input and textarea for note entry
@@ -61,25 +66,18 @@ $(document).on("click", "#savenote", function() {
   $("#bodyinput").val("");
 });
 
-// When you click the add note button
-// $(document).on("click", ".addnote", function() {
-//   let articleId = $(".title").attr("data-id");
-//   console.log(articleId);
-//   $("#savenote").data("id", articleId);
-//
-// });
-
 // Save article upon click
-// $(document).on("click", ".save-article", function() {
-//   let thisId = $(this).attr("data-id");
-//   $.ajax({
-//     method: "POST",
-//     url: "/articles/" + thisId,
-//     data: {
-//       saved: true
-//     }
-//   })
-//   .done(function(data) {
-//     console.log(data);
-//   });
-// });
+$(document).on("click", ".savearticle", function() {
+  let thisId = $(this).attr("data-id");
+  $.ajax({
+    method: "POST",
+    url: "/articles/" + thisId,
+    data: {
+      saved: true
+    }
+  })
+  .done(function(data) {
+    console.log(data);
+    $("a.savearticle").attr("data-id", data._id).hide();
+  });
+});
